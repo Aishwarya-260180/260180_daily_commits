5117f8da9dd98a527618e671cab1470e8b4babf1
Aishwarya-260180 <80842631+Aishwarya-260180@users.noreply.github.com>
2021-04-23 12:36:08 +0530

    Add files via upload

---
+++ Abbreviation.py
@@ -0,0 +1,11 @@
+string=input("Enter the Short-Form:")
+if(string=='lol'):
+    print("Laugh Out Loud")
+elif(string=='rofl'):
+    print("Rolling On The Floor Laughing")
+elif(string=='lmk'):
+    print("Let Me Know")
+elif(string=='smh'):
+    print ("Shaking My Head")
+else :
+    print("INVALID")
